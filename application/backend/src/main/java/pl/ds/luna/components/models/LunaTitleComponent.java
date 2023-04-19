f34b0cb503ce32e8edb6ff4a9c747a6ae05a124e
Paweł Jóźwicki <pawel.jozwicki@ds.pl>
2023-04-19 08:40:01 +0200

    Release 1.13.0

--- application/backend/src/main/java/pl/ds/luna/components/models/LunaTitleComponent.java
+++ application/backend/src/main/java/pl/ds/luna/components/models/LunaTitleComponent.java
@@ -16,11 +16,103 @@
 
 package pl.ds.luna.components.models;
 
+import javax.inject.Inject;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.Model;
-import pl.ds.howlite.components.models.TitleComponent;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import pl.ds.howlite.components.Grid;
+import pl.ds.howlite.components.Styled;
+import pl.ds.howlite.components.models.DefaultStyledGridComponent;
 
 @Model(adaptables = Resource.class)
-public class LunaTitleComponent extends TitleComponent {
+public class LunaTitleComponent implements Styled, Grid {
 
+  @Inject
+  @Default(values = "Add your heading here")
+  private String title;
+
+  @Inject
+  @Default(values = "h2")
+  private String headingLevel;
+
+  @Inject
+  @Default(values = "hl-title__heading--size-4")
+  private String headingSize;
+
+  @Inject
+  @Default(booleanValues = false)
+  private Boolean showSubtitle;
+
+  @Inject
+  @Default(values = "Add your text here")
+  private String subtitle;
+
+  @Inject
+  @Default(values = "")
+  private String anchorId;
+
+  @Self
+  private DefaultStyledGridComponent style;
+
+
+  @Override
+  public Integer getSmColSize() {
+    return style.getSmColSize();
+  }
+
+  @Override
+  public Integer getMdColSize() {
+    return style.getMdColSize();
+  }
+
+  @Override
+  public Integer getLgColSize() {
+    return style.getLgColSize();
+  }
+
+  @Override
+  public Integer getSmOffset() {
+    return style.getSmOffset();
+  }
+
+  @Override
+  public Integer getMdOffset() {
+    return style.getMdOffset();
+  }
+
+  @Override
+  public Integer getLgOffset() {
+    return style.getLgOffset();
+  }
+
+  @Override
+  public String[] getClasses() {
+    return style.getClasses();
+  }
+
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getHeadingLevel() {
+    return headingLevel;
+  }
+
+  public String getHeadingSize() {
+    return headingSize;
+  }
+
+  public Boolean getShowSubtitle() {
+    return showSubtitle;
+  }
+
+  public String getSubtitle() {
+    return subtitle;
+  }
+
+  public String getAnchorId() {
+    return anchorId;
+  }
 }
