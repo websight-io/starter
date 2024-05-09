6b9b78de8f58398632984dd5f0f0f10c47edcdce
Paweł Jóźwicki <pawel.jozwicki@ds.pl>
2024-05-09 16:58:28 +0200

    KYAN-219 Update styles and visual tests configuration

--- tests/end-to-end/backstop_data/engine_scripts/puppet/onReady.js
+++ tests/end-to-end/backstop_data/engine_scripts/puppet/onReady.js
@@ -1,5 +1,6 @@
 module.exports = async (page, scenario, vp) => {
   console.log('SCENARIO > ' + scenario.label);
+  await require('./preventLazyLoading')(page, scenario);
   await require('./clickAndHoverHelper')(page, scenario);
 
   // add more ready handlers here...
